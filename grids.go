0621e2bbb83ef1f9ecda95d0967bc5c00f0f70fa
Keith Peters <kp@bit-101.com>
2020-03-16 21:07:23 -0400

    rect and square layouts

--- grids.go
+++ grids.go
@@ -6,8 +6,8 @@ import (
 	"github.com/bit101/blgo"
 )
 
-// Rect draws a rectangular grid
-func Rect(surface *blgo.Surface, x, y, w, h, xres, yres float64) {
+// RectGrid draws a rectangular grid
+func RectGrid(surface *blgo.Surface, x, y, w, h, xres, yres float64) {
 	for y1 := y; y1 < y+h; y1 += yres {
 		surface.MoveTo(x, y1)
 		surface.LineTo(x+w, y1)
@@ -19,13 +19,13 @@ func Rect(surface *blgo.Surface, x, y, w, h, xres, yres float64) {
 	surface.Stroke()
 }
 
-// Square draws a square grid
-func Square(surface *blgo.Surface, x, y, w, h, res float64) {
-	Rect(surface, x, y, w, h, res, res)
+// SquareGrid draws a square grid
+func SquareGrid(surface *blgo.Surface, x, y, w, h, res float64) {
+	RectGrid(surface, x, y, w, h, res, res)
 }
 
-// Hex draws a hex grid
-func Hex(surface *blgo.Surface, x, y, w, h, r1, r2 float64) {
+// HexGrid draws a hex grid
+func HexGrid(surface *blgo.Surface, x, y, w, h, r1, r2 float64) {
 	HexLayout(x, y, w, h, r1, func(x1, y1 float64) {
 		surface.StrokePolygon(0, 0, r2, 6, math.Pi/2)
 	})
--- layouts.go
+++ layouts.go
@@ -7,6 +7,20 @@ import (
 // LayoutCallback is a function that gets called from a layout
 type LayoutCallback func(x, y float64)
 
+// RectLayout draws a rectangular grid
+func RectLayout(x, y, w, h, xres, yres float64, callback LayoutCallback) {
+	for y1 := y; y1 < y+h; y1 += yres {
+		for x1 := x; x1 < x+w; x1 += xres {
+			callback(x1+xres/2, y1+yres/2)
+		}
+	}
+}
+
+// SquareLayout draws a square grid
+func SquareLayout(x, y, w, h, res float64, callback LayoutCallback) {
+	RectLayout(x, y, w, h, res, res, callback)
+}
+
 // HexLayout calls a callback at every location in a hex grid
 func HexLayout(x, y, w, h, r float64, callback LayoutCallback) {
 	sin60r := math.Sin(math.Pi/3) * r
